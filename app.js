201719eb30219bba51ce856b7870c48801a29a99
Darya Khikhol <khikhol.darya@mail.ru>
2024-06-29 17:01:57 +0200

    app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const adsRoutes = require('./routes/ads');
-const { logRequest } = require('./utils/logger');
 const path = require('path');
 const { format } = require('date-fns');
 
